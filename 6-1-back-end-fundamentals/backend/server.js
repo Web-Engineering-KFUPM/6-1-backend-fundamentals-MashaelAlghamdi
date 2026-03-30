7240fbe5ad4f5e148a7f8ddcad87338cf37519e0
MashaelAlghamdi <s202253860@kfupm.edu.sa>
2026-03-30 11:39:52 +0300

    Completed TODO6]

--- 6-1-back-end-fundamentals/backend/server.js
+++ 6-1-back-end-fundamentals/backend/server.js
@@ -12,19 +12,17 @@ const app = express();
 app.use(cors());
 
 
-// TODO 4
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
 
 // TODO 5: Create the home route "/"
-
 app.get('/', (req, res) => {
   res.send('Hello from the back-end server');
 });
 
-// TODO 6: Create the "/about" route
 
+// TODO 6: Create the "/about" route
+app.get('/about', (req, res) => {
+  res.send('This is the about route');
+});
 
 
 // TODO 7: Create the "/student" route
@@ -32,3 +30,6 @@ app.get('/', (req, res) => {
 
 
 // TODO 4: Start the server on port 3000
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
